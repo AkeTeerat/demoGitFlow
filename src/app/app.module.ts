dd43bd5ab462c3622787a5733d75ae5ebcd1cb3c
Warakorn T <warakornt@ANANDAs-MacBook-Pro-2.local>
2023-07-25 13:41:46 +0700

    feat: feature 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Feature3Component } from './feature3/feature3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Feature3Component
   ],
   imports: [
     BrowserModule
